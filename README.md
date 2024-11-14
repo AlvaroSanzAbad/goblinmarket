dbf764f8eb46c120dc31586e0ae5fc325551fb60
Álvaro (Beesu) Sanz Abad <beesucontact@gmail.com>
2024-11-14 17:09:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# goblinmarket
+ Proyecto backend en java usando Spring. Tienda de venta de productos de todo tipo donde se organizan partidas y torneos de juegos.
---
+++ src/main/java/com/example/goblinmarket/GoblinmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.goblinmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoblinmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoblinmarketApplication.class, args);
+	}
+
+}
