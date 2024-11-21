9e0cdfccb4d1d1870c7e7007ad1b3aad4956e4c4
Álvaro (Beesu) Sanz Abad <beesucontact@gmail.com>
2024-11-21 21:24:06 +0100

    Usert insert y update

--- src/main/java/com/example/goblinmarket/user/UserController.java
+++ src/main/java/com/example/goblinmarket/user/UserController.java
@@ -1,16 +1,21 @@
 package com.example.goblinmarket.user;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.goblinmarket.user.dto.ResponseUserDTO;
 import com.example.goblinmarket.user.dto.ResponseUsersDTO;
+import com.example.goblinmarket.user.dto.UserDTO;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -31,6 +36,16 @@ public class UserController {
         return new ResponseUserDTO(userService.getUser(id));
     }
     
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseUserDTO insertUser(@RequestBody UserDTO u) {
+        return new ResponseUserDTO(userService.insertUser(u));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseUserDTO updateUser(@PathVariable int id, @RequestBody UserDTO userDTO) {
+        return new ResponseUserDTO(userService.updateUser(id, userDTO));
+    }
     
 
 }
--- src/main/java/com/example/goblinmarket/user/UserService.java
+++ src/main/java/com/example/goblinmarket/user/UserService.java
@@ -2,8 +2,11 @@ package com.example.goblinmarket.user;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.example.goblinmarket.user.dto.UserDTO;
 import com.example.goblinmarket.user.projections.UserWithNothing;
 
 import lombok.RequiredArgsConstructor;
@@ -19,5 +22,21 @@ public class UserService {
 
     public UserWithNothing getUser(int id){
         return userRepository.findUserById(id);
+    }
+
+    public UserWithNothing insertUser(UserDTO userDTO){
+        User user = userRepository.save(User.fromDTO(userDTO));
+        return userRepository.findUserById(user.getId());
+    }
+
+    public UserWithNothing updateUser(int id, UserDTO userDTO){
+        if (!userRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User's not found");
+        }
+        User user = User.fromDTO(userDTO);
+        user.setId(id);
+        userRepository.save(user);
+
+        return userRepository.findUserById(user.getId());
     }
 }
